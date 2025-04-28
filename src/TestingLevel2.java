851aa35d2c917b842de06adf78155a791cf4afde
Tsingh2027 <t_singh13@u.pacific.edu>
2025-04-27 20:04:46 -0700

    Chnage to level 2 pt 3

--- src/TestingLevel2.java
+++ src/TestingLevel2.java
@@ -17,11 +17,11 @@ public class TestingLevel2 extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int SIZE = 25;
 	public static final int MS = 25;
-	public static final int ENEMY_PROJ_SPEED = 20 ;
+	public static final int ENEMY_PROJ_SPEED = 30 ;
 	public static final int ENEMY_PROJ_SIZE = 11;
-	private final int USER_PROJ_SPEED = 7;
+	private final int USER_PROJ_SPEED = 10;
 	private final int USER_PROJ_SIZE = 8;
-	private static final int ENEMY_MOVE_SPEED = 50;
+	private static final int ENEMY_MOVE_SPEED = 40;
 
 	private int enemyShootCooldown = 0;
 	private int enemyTicksSinceLastShot = 0;
