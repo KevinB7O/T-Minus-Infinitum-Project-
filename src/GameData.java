ba52eaa195cdc6375156c74078a375e3b32bf467
tangl <tangl@10.15.211.123>
2025-04-27 19:50:27 -0700

    Fixed to the previous version

--- src/GameData.java
+++ src/GameData.java
@@ -1,27 +1,51 @@
 
 public class GameData {
-    private int totalScore = 0;
+	private int totalScore = 0;
     private int totalBonusPoints = 0;
-    private int totalElapsedTime = 0; // Stores time in seconds
-
-    private static GameData instance;
-
-    private GameData() { }
-
-    public static GameData getInstance() {
-        if (instance == null) {
-            instance = new GameData();
-        }
-        return instance;
+    private int totalElapsedTime = 0;
+	
+	private static GameData instance;
+    
+    /*public GameData (int totalScore, int totalBonusPoints) {
+    	this.totalScore = totalScore;
+    	this.totalBonusPoints = totalBonusPoints;
+    }*/
+	
+	private GameData() {
+		
+	}  
+	
+	public static GameData getInstance() {
+		if (instance == null) {
+			instance = new GameData();
+		}
+		return instance;
+	}
+    
+    
+    //Getter and setter
+    public int getTotalScore() {
+        return totalScore;
     }
-
-    // Getter and setter for totalElapsedTime
-    public long getTotalElapsedTime() {
-        return totalElapsedTime;
+    
+    public void setTotalScore (int totalScore) {
+    	this.totalScore = totalScore;
     }
-
-    public void addElapsedTime(long levelTime) {
-        totalElapsedTime += levelTime;
+    
+    //Getter and setter
+    public int getTotalBonusPoints() {
+        return totalBonusPoints;
     }
+    public void setTotalBonusPoints (int totalBonusPoints) {
+    	this.totalBonusPoints = totalBonusPoints;
+    }
+    
+    public void addScore(int score) {
+        totalScore += score;
+    }
+    
+    public void addBonus(int bonus) {
+        totalBonusPoints += bonus;
+    }
+    
 }
-
