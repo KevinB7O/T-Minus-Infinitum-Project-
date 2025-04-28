0f0c06be3e9463af053c40e2e8867b3ca030c3a8
Tsingh2027 <t_singh13@u.pacific.edu>
2025-04-27 20:02:03 -0700

    Change to level 2 pt 2.

--- src/TestingLevel2.java
+++ src/TestingLevel2.java
@@ -18,15 +18,15 @@ public class TestingLevel2 extends GraphicsProgram implements ActionListener {
 	public static final int SIZE = 25;
 	public static final int MS = 25;
 	public static final int ENEMY_PROJ_SPEED = 20 ;
-	public static final int ENEMY_PROJ_SIZE = 20;
+	public static final int ENEMY_PROJ_SIZE = 11;
 	private final int USER_PROJ_SPEED = 7;
 	private final int USER_PROJ_SIZE = 8;
-	private static final int ENEMY_MOVE_SPEED = 25;
+	private static final int ENEMY_MOVE_SPEED = 50;
 
 	private int enemyShootCooldown = 0;
 	private int enemyTicksSinceLastShot = 0;
 
-	private int mainShipShootCooldown = 13; // lower = faster shooting
+	private int mainShipShootCooldown = 10; // lower = faster shooting
 	private int mainShipTicksSinceLastShot = 0;
 
 	private int elapsedTime = 0; // time in seconds
